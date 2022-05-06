07fbfa18c979a4c26eee8392dc377bff2ad274d0
Hitesh Zala <hitesh.fluxbyte@gmail.com>
2022-05-06 16:09:31 +0530

    Crete element

--- Cart-Js/js/cartjs.js
+++ Cart-Js/js/cartjs.js
@@ -1,17 +1,42 @@
-let div = document.createElement('div');
+const addBtn = document.querySelector(".addBtn");
 
-div.id = 'headerid';
-div.className = 'headerclass';
+addBtn.addEventListener("click", () => {
+  console.log("We ara inside add  funbction");
+  // wrapper div
+  let wrapper_div = document.createElement("div");
+  wrapper_div.id = "wrapper-id";
+  wrapper_div.className = "wrapper-class";
 
-let label = document.createElement('label');
-label.textContent = "Header";
-div.appendChild(label);
+  //   header div
+  let header_div = document.createElement("div");
+  let header_label = document.createElement("label");
+  header_label.textContent = "Header";
+  header_div.appendChild(header_label);
 
+  header_div.id = "header-id";
+  header_div.className = "header-class";
+  wrapper_div.appendChild(header_div);
 
-let label = document.createElement('label');
-label.textContent = 'Description';
-div.appendChild(label);
+  //   description div
+  let description_div = document.createElement("div");
+  let description_label = document.createElement("label");
+  description_label.textContent = "Description";
+  description_div.appendChild(description_label);
 
-let script = document.createElement('script');
-script.src = './asset/img.jpg';
-div.appendChild(script);
+  description_div.id = "description-id";
+  description_div.class = "description-class";
+  wrapper_div.appendChild(description_div);
+
+  //   button div
+  let Btn_div = document.createElement("div");
+  let Btn_button = document.createElement("button");
+  Btn_div.id = "button-id";
+  Btn_div.className = "button-class";
+  Btn_button.innerHTML = "Add Data";
+  Btn_div.appendChild(Btn_button);
+  wrapper_div.appendChild(Btn_div);
+
+  const container = document.querySelector(".container");
+
+  container.append(wrapper_div);
+});
