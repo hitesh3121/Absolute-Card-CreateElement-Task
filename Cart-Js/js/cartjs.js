442c5004449e273502d1f9ab244ed2b72e4fb952
Hitesh <hitesh.fluxbyte@gmail.com>
2022-05-11 13:58:08 +0530

    CreateElement with JS

--- Cart-Js/js/cartjs.js
+++ Cart-Js/js/cartjs.js
@@ -1,3 +1,5 @@
+let dataArray = [];
+
 const addBtn = document.querySelector(".addBtn");
 
 addBtn.addEventListener("click", () => {
@@ -10,8 +12,11 @@ addBtn.addEventListener("click", () => {
   //   header div
   let header_div = document.createElement("div");
   let header_label = document.createElement("label");
+  let header_input = document.createElement('input');
+  header_input.id = "headerinput-id";
   header_label.textContent = "Header";
   header_div.appendChild(header_label);
+  header_div.appendChild(header_input);
 
   header_div.id = "header-id";
   header_div.className = "header-class";
@@ -20,8 +25,12 @@ addBtn.addEventListener("click", () => {
   //   description div
   let description_div = document.createElement("div");
   let description_label = document.createElement("label");
+  let descriptionb_input = document.createElement('input');
+
+  descriptionb_input.id = "descriptioninput-id";
   description_label.textContent = "Description";
   description_div.appendChild(description_label);
+  description_div.appendChild(descriptionb_input);
 
   description_div.id = "description-id";
   description_div.class = "description-class";
@@ -30,8 +39,8 @@ addBtn.addEventListener("click", () => {
   //   button div
   let Btn_div = document.createElement("div");
   let Btn_button = document.createElement("button");
-  Btn_div.id = "button-id";
-  Btn_div.className = "btn btn-success";
+  Btn_button.id = "button-id";
+  Btn_button.className = "btn btn-primary addDataBtn";
   Btn_button.innerHTML = "Add Data";
   Btn_div.appendChild(Btn_button);
   wrapper_div.appendChild(Btn_div);
@@ -39,4 +48,34 @@ addBtn.addEventListener("click", () => {
   const container = document.querySelector(".container");
 
   container.append(wrapper_div);
+  let addData = document.querySelector('.addDataBtn');
+  addData.addEventListener('click', () => {
+    console.log("Clicked Button");
+    let head_input = document.getElementById('headerinput-id');
+    let desc_input = document.getElementById('descriptioninput-id');
+
+    let data = {
+      header: head_input.value,
+      description: desc_input.value,
+    }
+    dataArray.push(data);
+
+    let HTMLcarddata = ``;
+
+    dataArray.forEach(function(singleObjects) {
+
+      HTMLcarddata = HTMLcarddata + `
+    <div class="second-container">
+        <div class="item1">
+            <div >${singleObjects.header}</div>
+        </div>
+        <div >${singleObjects.description}</div>
+    </div>`
+    })
+
+    head_input.value = '';
+    desc_input.value = '';
+    const show_card_data = document.querySelector('.show_card_data');
+    show_card_data.innerHTML = HTMLcarddata;
+  });
 });
