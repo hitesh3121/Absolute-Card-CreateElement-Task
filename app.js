f7f40daef919f99c9e750c3bfdaad6fa8bd1d12a
hitesh-fluxbyte <hitesh.fluxbyte@gmail.com>
2022-12-26 12:04:30 +0530

    figma practise

---
+++ app.js
@@ -0,0 +1,31 @@
+const dataArray = [
+    {
+        id: 1,
+        number: "55",
+        symbolText: 'No. of ULNs',
+        description: 'view details',
+        backgroundColor : "",
+    },
+    {
+        id: 2,
+        number: '66',
+        symbolText: 'ERRORS',
+        description: 'view details',
+        backgroundColor:""
+    },
+    {
+        id: 3,
+        number: '14%',
+        symbolText: '%ERRORS',
+        description: 'view details',
+        backgroundColor:""
+    },
+    {
+        id: 4,
+        number: '14%',
+        symbolText: '%ERRORS',
+        description: 'view details',
+        backgroundColor:""
+    }
+];
+
